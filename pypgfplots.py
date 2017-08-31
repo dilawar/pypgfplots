bf445af090319e0c83a8960462893d3190c9848e
Dilawar Singh <dilawars@ncbs.res.in>
2017-08-31 11:31:28 +0530

    nothing here.

---
+++ pypgfplots.py
@@ -0,0 +1,19 @@
+"""pypgfplots.py: 
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import numpy as np
+
+
+def write_tikzfigure( x, y, **kwargs ):
+    pass
+
