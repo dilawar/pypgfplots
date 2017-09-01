3d48b74b96bbc43eb5ecbb5d20e1412392cb8394
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-01 10:37:14 +0530

    Using PYTHONPATH to run tests.

--- tests/test_imshow.py
+++ tests/test_imshow.py
@@ -1,9 +1,8 @@
 import sys
-sys.path += [ '.', '..' ]
 import pypgfplots
 import numpy as np
-
 mat = np.random.rand( 10, 10 )
+
 pypgfplots.write_standalone_matrix( mat, outfile = '%s.tex' % sys.argv[0] 
         , title = 'Measurement matrix'
         , xlabel = 'Index'
