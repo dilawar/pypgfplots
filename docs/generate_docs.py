299e32bc4a2641e7db1d7db390325815ea0cd99c
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-02 11:00:12 +0530

    Added html page.

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -1,11 +1,10 @@
 import os
 
-docFile = 'index.md'
+docFile = 'index.html'
 
 files = [ ]
 for d, sd, fs in os.walk( '../tests/' ):
     for f in fs:
-        print( f )
         if '.py' == f[-3:]:
             files.append( os.path.join( d, f ) )
 
@@ -14,19 +13,20 @@ for f in files:
     with open( f, 'r' ) as _f:
         pytext.append( _f.read( ) ) 
 
-text =  [ '# Examples' ]
-text.append( "<table>" )
+text =  [ '<h1>Examples </h1>' ]
 
+text.append( "<table>" )
 for f, ftext in zip( files, pytext ):
     fname = os.path.basename( f )
     imgname = fname + '.png'
     url = '{{ site.url }} /tests/%s' % fname 
-    text.append( '## [%s](%s)\n\n' % (fname, url) )
-    text.append( '<tr><td>\n' )
-    text.append( '```python\n%s \n```' % ftext )
+    text.append( '<tr>' )
     text.append( '</td><td>' )
-    text.append( '\n![%s](%s)' % (fname, imgname ) )
-    text.append( '</td></tr>' )
+    text.append( '<pre>\n %s \n</pre>' % ftext )
+    text.append( '</td><td>' )
+    text.append( '<img src="%s">' % imgname )
+    text.append( '</td>' )
+    text.append( '</tr>' )
 
 text += [ "</table>" ]
 
